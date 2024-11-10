36a6c533143ff215fa030372420a8a8522f340a4
Ben Pollack <benpollack8723@gmail.com>
2024-11-10 16:03:01 -0600

    initial setup and intiial push

---
+++ server/routes/tarot.router.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const pool = require("../modules/pool");
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get("/", (req, res) => {
+  sqlQuery = `
+  SELECT * FROM "deck"
+  ORDER BY "id";
+  `;
+
+  pool
+    .query(sqlQuery)
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      console.log("Error fetching tarot deck:", err);
+      res.sendStatus(500);
+    });
+});
+
+/**
+ * POST route template
+ */
+router.post("/", async (req, res) => {
+    let connection;
+
+  try {
+    connection = await pool.connect();
+    await connection.query("BEGIN;");
+    
+   await Promise.all(
+    req.body.map((data) => {
+      const { name, suit, value, description } = data;
+      const sqlQuery = `
+        INSERT INTO "deck"
+            ("name", "suit", "value", "description")
+            VALUES
+            ($1, $2, $3, $4)
+            `;
+      const sqlValues = [name, suit, value, description];
+      return connection.query(sqlQuery, sqlValues)
+    })
+)
+
+    await connection.query("COMMIT;");
+    res.sendStatus(201);
+  } catch (err) {
+    console.log("POST /api/tarot failed:", err);
+    await connection.query("ROLLBACK;");
+    res.sendStatus(500);
+  } finally {
+    await connection.release();
+  }
+});
+
+module.exports = router;
